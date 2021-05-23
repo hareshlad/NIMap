6511feb12a07a2ce52e7312c1706edece9237656
hareshlad <55095077+hareshlad@users.noreply.github.com>
2021-05-23 15:19:11 +0530

    Update Question_1.py
    Add colon and space for input

--- Question_1.py
+++ Question_1.py
@@ -2,11 +2,11 @@ def sortListByLength(lst):
     lst.sort(key=len)
     return lst
 
-n=int(input("Enter number of strings "))
+n=int(input("Enter number of strings: "))
 if n<=0:
     print("Invalid Input")
 else:
-    input_string=input("Enter the list elements separated by comma")
+    input_string=input("Enter the list elements separated by comma: ")
     input_list= ['']*n
     input_list =  input_string.split(',')
     out_list=sortListByLength(input_list)
