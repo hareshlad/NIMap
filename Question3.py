ab072dd92bd226245d284d4e9317aa7afca92a87
hareshlad <55095077+hareshlad@users.noreply.github.com>
2021-05-23 16:24:41 +0530

    Update Question3.py
    Added condition for prime number

--- Question3.py
+++ Question3.py
@@ -3,18 +3,25 @@ import math
 
 def sumofFactors(n) :
     list_factors=[]
+    prime_flag=True
     sum =0
     for i in range(1, n + 1):
        if n % i == 0 and i%2==0:
+           prime_flag=False
            list_factors.append(i)
            sum = sum + i
-     
-    list_factors.sort(reverse=True)
-    print(list_factors)
-    print(sum)
+    if prime_flag:
+        print("Enter a different number")
+    else:
+        list_factors.sort(reverse=True)
+        print(list_factors)
+        print(sum)
     
     
  
 
 n = int(input("Enter a number :"))
-sumofFactors(n)
+if(n<10):
+    print('Invalid Input')
+else:
+    sumofFactors(n)
